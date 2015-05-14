24758bd08f370a853b02b5f99c55c4c03bf8a65a
Jeremy Shaw <jeremy@n-heptane.com>
2015-05-14 12:42:05 -0500

    default.nix use makeWrapper to ensure that hackage-whatsnew can find cabal-install and ghc

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 { mkDerivation, base, Cabal, containers, directory, filepath
-, hackage-db, process, stdenv, temporary
+, hackage-db, process, stdenv, temporary, cabal-install, ghc
+, makeWrapper
 }:
-mkDerivation {
+mkDerivation rec {
   pname = "hackage-whatsnew";
   version = "0.1.0.0";
   src = ./.;
@@ -11,5 +12,12 @@ mkDerivation {
     base Cabal containers directory filepath hackage-db process
     temporary
   ];
+  buildTools = [ makeWrapper ];
+  postInstall = ''
+    exe=$out/libexec/${pname}-${version}/hackage-whatsnew
+    install -D $out/bin/hackage-whatsnew $exe
+    rm -rf $out/{bin,lib,share}
+    makeWrapper $exe $out/bin/hackage-whatsnew --prefix PATH ":" "${cabal-install}/bin:${ghc}/bin"
+  '';
   license = stdenv.lib.licenses.bsd3;
 }
